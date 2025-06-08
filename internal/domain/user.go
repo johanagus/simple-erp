e8dae28098d27edffaf698749347b135d136fe92
Johan Agus <johanagus.it@gmail.com>
2025-06-09 00:03:31 +0700

    fix : add tag json and validation

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -3,13 +3,13 @@ package domain
 import "time"
 
 type User struct {
-	ID        uint   `gorm:"primaryKey"`
-	Email     string `gorm:"type:varchar(50)"`
-	Phone     string `gorm:"type:varchar(14)"`
-	Firstname string `gorm:"type:varchar(10)"`
-	Lastname  string `gorm:"type:varchar(10)"`
-	Password  string `gorm:"type:varchar(255)"`
-	CompanyId int
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Email     string    `gorm:"type:varchar(50)" json:"email" validate:"required,email"`
+	Phone     string    `gorm:"type:varchar(14)" json:"phone"`
+	Firstname string    `gorm:"type:varchar(10)" json:"firstname"`
+	Lastname  string    `gorm:"type:varchar(10)" json:"lastname"`
+	Password  string    `gorm:"type:varchar(255)" json:"password,omitempty"`
+	CompanyId int       `json:"company_id"`
+	CreatedAt time.Time `json:"-"` // tidak tampilkan di respon api
+	UpdatedAt time.Time `json:"-"` // tidak tampilkan di respon api
 }
