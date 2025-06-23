e2e7310c055704716e7acc8d74d1d5b1a0b8cbbe
Johan Agus <johanagus.it@gmail.com>
2025-06-23 11:01:18 +0700

    fix : simplify product roles

--- internal/routes/product_router.go
+++ internal/routes/product_router.go
@@ -7,10 +7,10 @@ import (
 )
 
 func RegisterProductRoutes(router fiber.Router, h *handler.ProductHandler) {
-	router.Get("/products", middleware.RequirePermission("get_products"), h.FindAll)
-	router.Get("/product/:id", middleware.RequirePermission("get_product_by_id"), h.FindByID)
-	router.Get("/product/sku/:sku", middleware.RequirePermission("get_product_by_sku"), h.FindBySKU)
-	router.Get("/product/barcode/:barcode", middleware.RequirePermission("get_product_by_barcode"), h.FindByBarcode)
+	router.Get("/products", middleware.RequirePermission("get_product"), h.FindAll)
+	router.Get("/product/:id", middleware.RequirePermission("get_product"), h.FindByID)
+	router.Get("/product/sku/:sku", middleware.RequirePermission("get_product"), h.FindBySKU)
+	router.Get("/product/barcode/:barcode", middleware.RequirePermission("get_product"), h.FindByBarcode)
 	router.Get("/products/search", middleware.RequirePermission("search_product"), h.Search)
 	router.Post("/product", middleware.RequirePermission("create_product"), h.SaveProduct)
 	router.Put("/product", middleware.RequirePermission("update_products"), h.UpdateProduct)
