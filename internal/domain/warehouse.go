6a296f6274013019b65726291076da098f493127
Johan Agus <johanagus.it@gmail.com>
2025-06-27 19:36:40 +0700

    fix[warehouse] : update permission and add tag created_at and updated_at

--- internal/domain/warehouse.go
+++ internal/domain/warehouse.go
@@ -3,11 +3,11 @@ package domain
 import "time"
 
 type Warehouse struct {
-	ID        uint   `gorm:"primaryKey;autoIncrement"`
-	CompanyID uint   `gorm:"not null"`                     // Foreign key to Company
-	StoreID   uint   `gorm:"not null" validate:"required"` // Foreign key to Store
-	Name      string `gorm:"type:varchar(100)" validate:"required"`
-	Address   string `gorm:"type:varchar(255)" validate:"required"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        uint      `gorm:"primaryKey;autoIncrement"`
+	CompanyID uint      `gorm:"not null"`                     // Foreign key to Company
+	StoreID   uint      `gorm:"not null" validate:"required"` // Foreign key to Store
+	Name      string    `gorm:"type:varchar(100)" validate:"required"`
+	Address   string    `gorm:"type:varchar(255)" validate:"required"`
+	CreatedAt time.Time `gorm:"<-:create"`
+	UpdatedAt time.Time `gorm:"<-:update"`
 }
--- internal/routes/warehouse_router.go
+++ internal/routes/warehouse_router.go
@@ -8,7 +8,7 @@ import (
 
 func RegisterWarehouseRoutes(router fiber.Router, h *handler.WarehouseHandler) {
 
-	router.Get("/warehouses", middleware.RequirePermission("get_warehouses"), h.FindAllWarehouses)
+	router.Get("/warehouses", middleware.RequirePermission("get_warehouse"), h.FindAllWarehouses)
 	router.Get("/warehouse/:id", middleware.RequirePermission("get_warehouse"), h.FindWarehouseByID)
 	router.Post("/warehouse", middleware.RequirePermission("create_warehouse"), h.CreateWarehouse)
 	router.Put("/warehouse/:id", middleware.RequirePermission("update_warehouse"), h.UpdateWarehouse)
