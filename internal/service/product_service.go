1acfcc93f4573c3a98351663c5817258b31d0776
Johan Agus <johanagus.it@gmail.com>
2025-07-14 19:36:47 +0700

    fix : add product sku generator

--- internal/service/product_service.go
+++ internal/service/product_service.go
@@ -2,8 +2,11 @@ package service
 
 import (
 	"errors"
+	"fmt"
+	"time"
 
 	"github.com/johanagus/simple-erp/internal/domain"
+	"github.com/johanagus/simple-erp/internal/dto"
 	"github.com/johanagus/simple-erp/internal/repository"
 )
 
@@ -13,20 +16,24 @@ type ProductService interface {
 	FindBySKU(sku string) (*domain.Product, error)
 	FindByBarcode(barcode string) (*domain.Product, error)
 	Search(query string) ([]domain.Product, error)
-	SaveProduct(product *domain.Product) (int, error)
+	SaveProduct(p *dto.ProductRequest) (int, error)
 	UpdateProduct(id int, product *domain.Product) (*domain.Product, error)
 }
 
 type productServiceImpl struct {
-	repo repository.ProductRepository
+	productRepo  repository.ProductRepository
+	categoryRepo repository.CategoryRepository
 }
 
-func NewProductService(repo repository.ProductRepository) ProductService {
-	return &productServiceImpl{repo: repo}
+func NewProductService(productRepo repository.ProductRepository, categoryRepo repository.CategoryRepository) ProductService {
+	return &productServiceImpl{
+		productRepo:  productRepo,
+		categoryRepo: categoryRepo,
+	}
 }
 
 func (s *productServiceImpl) FindAll() ([]domain.Product, error) {
-	products, err := s.repo.FindAll()
+	products, err := s.productRepo.FindAll()
 	if err != nil {
 		return nil, err
 	}
@@ -35,30 +42,72 @@ func (s *productServiceImpl) FindAll() ([]domain.Product, error) {
 }
 
 func (s *productServiceImpl) FindByID(id int) (*domain.Product, error) {
-	return s.repo.FindByID(id)
+	return s.productRepo.FindByID(id)
 }
 
 func (s *productServiceImpl) FindBySKU(sku string) (*domain.Product, error) {
-	return s.repo.FindBySKU(sku)
+	return s.productRepo.FindBySKU(sku)
 }
 
 func (s *productServiceImpl) FindByBarcode(barcode string) (*domain.Product, error) {
-	return s.repo.FindByBarcode(barcode)
+	return s.productRepo.FindByBarcode(barcode)
 }
 
 func (s *productServiceImpl) Search(query string) ([]domain.Product, error) {
-	return s.repo.Search(query)
+	return s.productRepo.Search(query)
 }
 
-func (s *productServiceImpl) SaveProduct(product *domain.Product) (int, error) {
-	sku := product.SKU
-	res, err := s.repo.FindBySKU(sku)
+func (s *productServiceImpl) SaveProduct(p *dto.ProductRequest) (int, error) {
+	now := time.Now()
+	year := now.Year()
+	month := int(now.Month())
+
+	// Find Category
+	category, err := s.categoryRepo.FindByName(p.Category)
+	if err != nil {
+		return 0, err
+	}
+
+	if category == nil {
+		return 0, errors.New("category not found")
+	}
+
+	var urutan int
+
+	LastSKUID := s.productRepo.FindLastSKUByCategoryAndPeriod(year, month, category.ID)
+
+	if LastSKUID != "" && len(LastSKUID) >= 4 {
+		fmt.Printf("jalan")
+		// Ambil 4 digit terakhir dari SKU terakhir
+		fmt.Sscanf(LastSKUID[len(LastSKUID)-4:], "%04d", &urutan)
+	}
+
+	urutan++
+
+	fmt.Println(urutan)
+
+	sku := fmt.Sprintf("%02d%02d%d%04d", year%100, month, category.ID, urutan)
+
+	product := &domain.Product{
+		SKU:         sku,
+		Barcode:     p.Barcode,
+		Name:        p.Name,
+		CategoryID:  category.ID,
+		Type:        p.Type,
+		Brand:       p.Brand,
+		PriceLevel1: p.Price,
+		IsSN:        p.IsSN,
+	}
+
+	// Pastikan SKU unik
+	res, err := s.productRepo.FindBySKU(sku)
 	if err == nil && res != nil {
 		return 0, errors.New("sku already exists")
 	}
-	return s.repo.SaveProduct(product)
+
+	return s.productRepo.SaveProduct(product)
 }
 
 func (s *productServiceImpl) UpdateProduct(id int, product *domain.Product) (*domain.Product, error) {
-	return s.repo.UpdateProduct(id, product)
+	return s.productRepo.UpdateProduct(id, product)
 }
